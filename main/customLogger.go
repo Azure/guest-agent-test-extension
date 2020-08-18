115854d5e66579f04c64ddfaf145283228c55256
t-etfali <t-etfali@microsoft.com>
2020-08-18 16:48:42 -0700

    Minor comments on logging

--- main/customLogger.go
+++ main/customLogger.go
@@ -37,7 +37,12 @@ func (l *customLogger) Printf(format string, v ...interface{}) {
 
 func formatMessage(messageType string, message string) string {
 	currentTime := time.Now().UTC().Format("2006-01-02T15:04:05.999Z")
+
+	//Caller(1) is the customlogger Print function in this file
+	//Caller(2) is the line that called the logger's print function
 	_, filePath, line, _ := runtime.Caller(2)
+
+	//Gets the line number and caller of the log message to identify what line logged
 	shortFileArr := strings.Split(filePath, "/")
 	shortFile := shortFileArr[len(shortFileArr)-1]
 
