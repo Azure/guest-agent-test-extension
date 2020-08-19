586b3d76e4d6e94d0732e58c1eb1dbdc652d1fdf
t-etfali <t-etfali@microsoft.com>
2020-08-19 09:45:09 -0700

    Minor error fixing pt2

--- main/main.go
+++ main/main.go
@@ -241,7 +241,7 @@ func initLogging() (*os.File, error) {
 	if err != nil {
 		return nil, errors.Wrap(err, "Failed to open handler environment")
 	}
-	logfileLogname := extensionName + "-" + version
+	logfileLogName := extensionName + "-" + version
 	logfile = path.Join(handlerEnv.HandlerEnvironment.LogFolder, logfileLogName)
 
 	file, err := os.OpenFile(logfile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
