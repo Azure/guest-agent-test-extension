a3e28be0e8a794ad8eb599d267c4f19705bcced2
Nageswara Nandigam <nnandigam@microsoft.com>
2021-12-20 23:11:05 -0800

    change file permissions

--- scripts/service_install.sh
+++ scripts/service_install.sh
@@ -5,5 +5,5 @@ service_name="gatestext.service"
 
 dir=$(pwd)
 eval "sed s#{WORKDIR}#$dir# $dir/services/gatestext.service > /lib/systemd/system/gatestext.service"
-chmod 755 /lib/systemd/system/$service_name
+chmod 644 /lib/systemd/system/$service_name
 systemctl daemon-reload
