b159b5ca4c7ecca02b2404863f67b12fa055adaf
Nageswara Nandigam <nnandigam@microsoft.com>
2021-12-27 15:54:10 -0800

    update file location

--- scripts/service_install.sh
+++ scripts/service_install.sh
@@ -4,6 +4,7 @@ set -euxo pipefail
 service_name="gatestext.service"
 
 dir=$(pwd)
+cp $dir/bin/gatestext_script_linux /usr/local/bin
 eval "sed s#{WORKDIR}#$dir# $dir/services/gatestext.service > /lib/systemd/system/gatestext.service"
 chmod 644 /lib/systemd/system/$service_name
 systemctl daemon-reload
